577751de467fbdbd37f543c2e60d656a167f00dc
KonnerGann <konneregann@gmail.com>
2025-02-04 09:19:58 -0600

    Update converter.js

--- GitHub Projects/Programming/Javascript/temp converter/converter.js
+++ GitHub Projects/Programming/Javascript/temp converter/converter.js
@@ -1,12 +1,8 @@
-function cConverter() 
-{
-    //variables//
-    let ogTemp = document.getElementById("temp");
-    let result = documnet.getElementById("result");
-    let output = null
-    //If Function//
-    if (output = null) {
-        output = (ogTemp - 32) * 5 / 9
-    }
-    result.textContent = `${ogTemp}F is ${output}C`;
-};
+function toCelcius() 
+    {let ogTemp = document.getElementById("temp").value;
+    let output = (ogTemp - 32) * (5/9)
+    document.getElementById("result").textContent = `${ogTemp}F is ${output}C.`;}
+function toKelvin() 
+    {let ogTemp = document.getElementById("temp").value;
+    let output = (ogTemp - 32) * (5/9) + 273.15;
+    document.getElementById("result").textContent = `${ogTemp}F is ${output}K.`;}
